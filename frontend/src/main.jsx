00ad65fc73c5fe52b111495e04689e37c624d423
AahadKhan-code <aahadkhan2715@gmail.com>
2025-10-27 18:59:06 +0530

    pre-final

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="211507124105-l8hvsu0rpcl8t71h6rhfutdtdr3uf94q.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <App />
   </GoogleOAuthProvider>
 );
