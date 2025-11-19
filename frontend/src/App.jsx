4849710d25bfd9ea4914e0bafc47be7d9d97e038
AahadKhan-code <aahadkhan2715@gmail.com>
2025-11-19 15:37:25 +0530

    fixed page limit bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,6 @@ import PageTrackingWrapper from "./components/PageTrackingWrapper";
 import AdminRoute from "./components/AdminRoute";
 
 import Home from "./pages/Home";
-import Footer from "./components/Footer";
-
 // Lazy-loaded components
 const Navbar = lazy(() => import("./components/Navbar"));
 const JobDetails = lazy(() => import("./pages/JobDetails"));
